afffa2c629f5c5dd4c38fa92bb72db72ffb7481a
gubesch <christian.gubesch@gmail.com>
2021-11-20 11:42:15 +0100

    [CI Test] Last commit before tagging IT02 - refined

--- src/test/java/org/c02/iot/effect/test/SpinningLedTest.java
+++ src/test/java/org/c02/iot/effect/test/SpinningLedTest.java
@@ -24,7 +24,7 @@ public class SpinningLedTest {
         InOrder inOrder = Mockito.inOrder(buttonInstance);
         for (int j = 1; j <= 4; j++) {
             for (int i = 1; i <= 11; i++) {
-                inOrder.verify(buttonInstance).setLed(i, Color.green);
+                inOrder.verify(buttonInstance).setLed(i, Color.blue);
                 inOrder.verify(buttonInstance).allLedsOff();
             }
         }
