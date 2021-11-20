c485310143e4bfd4be1c757da71cc5199cfe689b
Novflo06 <florian.novosel@edu.campus02.at>
2021-11-20 14:40:13 +0100

    [CI] Added functional test for ClockUtilTest

--- src/test/java/org/c02/iot/clock/test/ClockUtilTest.java
+++ src/test/java/org/c02/iot/clock/test/ClockUtilTest.java
@@ -1,12 +1,41 @@
 package org.c02.iot.clock.test;
 
+import org.c02.swe.iot.Button;
+import org.c02.swe.iot.IButton;
+import org.c02.swe.iot.LedStatus;
+import org.c02.swe.iot.clock.ClockUtil;
+import org.c02.swe.iot.cloud.api.IParticleApi;
 import org.c02.swe.iot.cloud.api.ParticleException;
+import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class ClockUtilTest {
 
+
     @Test
     public void testShow() throws ParticleException {
+        IButton buttonInstance = Mockito.mock(IButton.class);
 
+        ClockUtil clockUtil = new ClockUtil(buttonInstance);
+
+        Date testdate = new Date();
+        testdate.setHours(3);
+        testdate.setMinutes(30);
+        testdate.setSeconds(45);
+
+        clockUtil.show(testdate);
+
+        List<LedStatus> ledStatusList = new ArrayList<>();
+        ledStatusList.add(new LedStatus(3, Color.red));
+        ledStatusList.add(new LedStatus(6, Color.green));
+        ledStatusList.add(new LedStatus(9, Color.blue));
+
+        Mockito.verify(buttonInstance).setLeds(ledStatusList);
     }
 }
